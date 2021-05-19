f927f96ed39fd5044a658dd7e678e6dc4188d881
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-05-19 23:45:27 +0200

    build: Server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+require("dotenv").config();
+const path = require('path');
+const app = express();
+
+app.use(cors());
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Import Routes
+const userRoutes = require("./routes/users.routes.js");
+const postRoutes = require("./routes/post.routes.js");
+
+// Route MiddLewares
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(userRoutes);
+app.use(postRoutes);
+
+
+
+
+// set port, listen for requests
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.");
+});
