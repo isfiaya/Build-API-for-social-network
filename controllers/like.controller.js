de0136cbbb915c1a3cd090784fd7a8cb09137ef7
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-05-21 15:15:25 +0200

    feat : user can't like post twice

--- controllers/like.controller.js
+++ controllers/like.controller.js
@@ -5,16 +5,28 @@ exports.sendLike = async (req, res) => {
     const userId = req.body.userId;
     const postId = req.body.postId;
 
-    connection.query("INSERT INTO likes SET userId=? , postId=?", [userId, postId], (error, results) => {
+    // CHECK THE USER IF HE ALREADY LIKE THE POST OR NOT 
+
+    connection.query("SELECT * FROM likes WHERE userId=? AND postId=?", [userId, postId], (error, results) => {
         if (error) {
             console.log(error);
             return res.status(400).json({
                 message: "error in like system"
             })
         }
-        if (results) {
+        if (results.length) {
             return res.status(200).json({
-                message: "thanks for like"
+                message: "you can't like the same post twice!"
+            })
+        }
+        if (!results.length) {
+            connection.query("INSERT INTO likes SET userId=? , postId=?", [userId, postId], (error, results) => {
+                if (error) {
+                    res.send(error)
+                }
+                if (results) {
+                    res.status(200).send("thanks for like !")
+                }
             })
         }
     })
