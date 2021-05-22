a7666a08fb4cd2dfa7aab1cd1c687aa438413d2a
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-05-22 17:44:54 +0200

    try: remove comment

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -2,47 +2,47 @@ const connection = require('../config/db.config');
 
 
 exports.submitComment = async (req, res) => {
-    const comment = req.body.comment;
-    const userId = req.body.userId;
-    const postId = req.body.postId;
+  const comment = req.body.comment;
+  const userId = req.body.userId;
+  const postId = req.body.postId;
 
-    connection.query("INSERT INTO comments SET comment=? , userId=? , postId=?", [comment, userId, postId], (error, results) => {
-        if (error) {
-            res.status(400).send({
-                message: "problem in comment system"
-            })
-        }
-        if (results) {
-            res.status(200).send({
-                message: "thank you for your submission comment"
-            })
-        }
-    })
+  connection.query("INSERT INTO comments SET comment=? , userId=? , postId=?", [comment, userId, postId], (error, results) => {
+    if (error) {
+      res.status(400).send({
+        message: "problem in comment system"
+      })
+    }
+    if (results) {
+      res.status(200).send({
+        message: "thank you for your submission comment"
+      })
+    }
+  })
 }
 
 exports.getComment = async (req, res) => {
 
-    connection.query("SELECT * FROM comments", (error, results) => {
-        if (error) {
-            res.status(400).send(error)
-        }
-        if (results) {
-            res.status(200).send(results)
-        }
-    })
+  connection.query("SELECT * FROM comments", (error, results) => {
+    if (error) {
+      res.status(400).send(error)
+    }
+    if (results) {
+      res.status(200).send(results)
+    }
+  })
 
 }
 
 exports.deleteComment = async (req, res) => {
-    const id = req.body.id
-    connection.query("DELETE FROM comments WHERE id=?", id, (error, results) => {
-        if (error) {
-            res.status(400).send(error)
-        }
-        if (results) {
-            res.status(200).send({
-                message: "comment deleted!"
-            })
-        }
-    })
+  const id = req.body.idComment;
+  connection.query(`DELETE FROM comments WHERE id = ?`, [id], (error, results) => {
+    if (error) {
+      return res.status(400).json(error)
+    }
+    if (results) {
+      return res.status(200).json({
+        message: "comment deleted!"
+      })
+    }
+  })
 }
