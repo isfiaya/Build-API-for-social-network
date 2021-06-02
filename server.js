090650753c58e1a0ec80702b5e5f6967266447b4
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-06-02 23:06:39 +0200

    fix: Cors

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader('Acces-Control-Allow-Origin', '*');
-    res.setHeader('Acces-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Acces-Contorl-Allow-Methods', 'Content-Type', 'Authorization');
-    next();
-})
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
