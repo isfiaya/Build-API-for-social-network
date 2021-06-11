45a66ba0f8321df8cee2ca24ebe31619d85e0109
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-06-11 17:40:54 +0200

    feat: Connect api with remote mysql database
    TODO: Images should be separated from API

--- config/db.config.js
+++ config/db.config.js
@@ -2,17 +2,17 @@ const mysql = require("mysql");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-    host: "sql4.freesqldatabase.com",
-    user: "sql4417165",
-    password: "KrEsm16xyJ",
-    database: "sql4417165",
-    port: 3306
+  host: "sql4.freesqldatabase.com",
+  user: "sql4417165",
+  password: "KrEsm16xyJ",
+  database: "sql4417165",
+  port: 3306
 });
 
 // open the MySQL connection
 connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
 });
 
 module.exports = connection;
