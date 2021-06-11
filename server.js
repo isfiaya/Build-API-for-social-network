d2938e658704b971a26b217f3f53f083e6e050ce
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-06-11 17:43:16 +0200

    fix: Express static port on Heroku
    Change the '3000' port with dynamic/environment value first

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use("/home/comment", commentRoutes);
 
 
 // set port, listen for requests
-app.listen(3000, () => {
-    console.log("Server is running on port 3000.");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
